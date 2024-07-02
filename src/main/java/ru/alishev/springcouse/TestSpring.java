893249ced5fc9eeb0e5b7cddf73eb5e68c2bcf0d
Yana Yasinvski <jantschick3@gmail.com>
2024-07-02 21:51:41 +0200

    lesson-6: DI 2

--- src/main/java/ru/alishev/springcouse/TestSpring.java
+++ src/main/java/ru/alishev/springcouse/TestSpring.java
@@ -12,8 +12,20 @@ public class TestSpring {
 
         // MusicPlayer musicPlayer = new MusicPlayer(musicBean);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
