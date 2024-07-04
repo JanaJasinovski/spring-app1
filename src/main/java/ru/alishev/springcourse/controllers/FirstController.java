c4e94d408dd6bc8664acfc3a467d98c881d9a222
Yana Yasinvski <jantschick3@gmail.com>
2024-07-04 18:07:59 +0200

    SpringMVC-6: @RequestParam

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -3,13 +3,18 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
