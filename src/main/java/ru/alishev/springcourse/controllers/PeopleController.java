8aa388fbd80e2d5ce22a41e4afa14eb045ceb977
Yana Yasinvski <jantschick3@gmail.com>
2024-07-08 17:34:15 +0200

    SpringMVC-8: FirstRest

---
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -0,0 +1,29 @@
+package ru.alishev.springcourse.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+@RequiredArgsConstructor
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
